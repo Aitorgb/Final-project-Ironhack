97c13d7d42d25d0386be740ddef16b6e7ea662bd
aitorgb <aitorguerrero9@gmail.com>
2020-10-23 10:56:18 +0200

    Feat: Reviews change

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -2,68 +2,67 @@ const createError = require('http-errors');
 const Review = require('../models/review.model');
 const Booking = require('../models/booking.model');
 const { param } = require('../config/routes');
+const { date } = require('faker');
 
 module.exports.createReview = (req, res, next) => {
+	const review = req.body;
+	const dateNow = new date.now();
+	const params = {
+		user: req.session.user.id,
+		space: req.params.id,
+		//    booking: req.params.id,
+	};
 
-  const params = { 
-    user: req.session.user.id,
-    space: req.params.id,
-//    booking: req.params.id,
-    review: req.body
-  }
-
-  Booking.findOne(params)
-    .then(booking => {
-      if(!booking) {
-        throw createError(403, "user doesn´t have enough permissions to proceed further")
-      } else {
-        const newReview = new Review(params)
-        newReview.save()
-          .then((review) => res.json(review))
-          .catch((e) => createError(400, e))
-      }
-  })
-  .catch((e) => createError(400, e))
-}
-
-module.exports.deleteReview = (req, res, next) => {
-	const spaceId = req.params.id;
-
-	Review.findByIdAndDelete(spaceId)
-			.then((review) => {
-				if(review) {
-					res.status(200).json()
-				} else {
-					next(createError(403, "user doesn´t have enough permissions to proceed further"))
+	Booking.find(params)
+		.then((bookings) => {
+			if (!bookings) {
+				throw createError(403, 'user doesn´t have enough permissions to proceed further');
+			} else {
+				const bookingPast = bookings.filter((booking) => booking.checkOut < dateNow);
+				if (bookingPast.length > 0) {
+					const newReview = new Review(review);
+					newReview.save().then((review) => res.status(200).json(review)).catch((e) => createError(400, e));
 				}
 			}
-			).catch((e) => createError(400, e))
+		})
+		.catch((e) => next(createError(400, e)));
+};
+
+module.exports.deleteReview = (req, res, next) => {
+	const reviewId = req.params.id;
+
+	Review.findByIdAndDelete(reviewId)
+		.then((review) => {
+			if (review) {
+				res.status(200).json();
+			} else {
+				next(createError(403, 'user doesn´t have enough permissions to proceed further'));
+			}
+		})
+		.catch((e) => next(createError(400, e)));
 };
 
 module.exports.editReview = (req, res, next) => {
-	const params = { 
-    user: req.session.user.id,
-    space: req.params.id,
-//    booking: req.params.id,
-    review: req.body
-  }
+	const params = {
+		user: req.session.user.id,
+		space: req.params.id,
+		//    booking: req.params.id,
+		review: req.body
+	};
 
 	Review.findOne(params)
-		.then(review => {
-			review = req.body
-			review.save()
-				.then(review => res.status(200).json(review))
-				.catch((e) => createError(400, e))
+		.then((review) => {
+			review = req.body;
+			review.save().then((review) => res.status(200).json(review)).catch((e) => createError(400, e));
 		})
-		.catch((e) => createError(400, e))
+		.catch((e) => createError(400, e));
 };
-
 
 //Controlador para pedir todos las reviews de un usuario
 module.exports.reviews = (req, res, next) => {
-  Review.find(req.params.id)
-    .then(reviews => {
-      res.status(200).json(reviews)
-    })
-    .catch((e) => createError(400, e))
-}
+	Review.find(req.params.id)
+		.then((reviews) => {
+			res.status(200).json(reviews);
+		})
+		.catch((e) => createError(400, e));
+};
